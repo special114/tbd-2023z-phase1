b6e4bc858e4f961c9ddc09fac6f1a10f50d56770
Rafał Surdej <special114@mailbox.org>
2023-12-21 21:07:17 +0100

    Change main.tf for v1.34

--- main.tf
+++ main.tf
@@ -4,13 +4,18 @@ locals {
   notebook_subnet_name    = "subnet-01"
   notebook_subnet_id      = "${var.region}/${local.notebook_subnet_name}"
   composer_subnet_address = "10.11.0.0/16"
+  composer_work_namespace = "composer-user-workloads"
   code_bucket_name        = "${var.project_name}-code"
   data_bucket_name        = "${var.project_name}-data"
   spark_version           = "3.3.2"
+  spark_driver_port       = 30000
+  spark_blockmgr_port     = 30001
+  dbt_version             = "1.7.3"
+  dbt_spark_version       = "1.7.1"
 }
 
 module "vpc" {
-  source         = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/vpc"
+  source         = "./modules/vpc"
   project_name   = var.project_name
   region         = var.region
   network_name   = local.notebook_vpc_name
@@ -20,22 +25,24 @@ module "vpc" {
 
 
 module "gcr" {
-  source       = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/gcr"
+  source       = "./modules/gcr"
   project_name = var.project_name
 }
 
 module "jupyter_docker_image" {
   depends_on         = [module.gcr]
-  source             = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/jupyter_docker_image"
+  source             = "./modules/jupyter_docker_image"
   registry_hostname  = module.gcr.registry_hostname
   registry_repo_name = coalesce(var.project_name)
   project_name       = var.project_name
   spark_version      = local.spark_version
+  dbt_version        = local.dbt_version
+  dbt_spark_version  = local.dbt_spark_version
 }
 
 module "vertex_ai_workbench" {
   depends_on   = [module.jupyter_docker_image, module.vpc]
-  source       = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/vertex-ai-workbench"
+  source       = "./modules/vertex-ai-workbench"
   project_name = var.project_name
   region       = var.region
   network      = module.vpc.network.network_id
@@ -52,7 +59,7 @@ module "vertex_ai_workbench" {
 #
 module "dataproc" {
   depends_on   = [module.vpc]
-  source       = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/dataproc"
+  source       = "./modules/dataproc"
   project_name = var.project_name
   region       = var.region
   subnet       = module.vpc.subnets[local.notebook_subnet_id].id
@@ -61,7 +68,7 @@ module "dataproc" {
 
 ## Uncomment for Dataproc batches (serverless)
 #module "metastore" {
-#  source = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/metastore"
+#  source = "./modules/metastore"
 #  project_name   = var.project_name
 #  region         = var.region
 #  network        = module.vpc.network.network_id
@@ -69,7 +76,7 @@ module "dataproc" {
 
 module "composer" {
   depends_on     = [module.vpc]
-  source         = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/composer"
+  source         = "./modules/composer"
   project_name   = var.project_name
   network        = module.vpc.network.network_name
   subnet_address = local.composer_subnet_address
@@ -78,21 +85,23 @@ module "composer" {
     "AIRFLOW_VAR_REGION_NAME" : var.region,
     "AIRFLOW_VAR_BUCKET_NAME" : local.code_bucket_name
     "AIRFLOW_VAR_PHS_CLUSTER" : module.dataproc.dataproc_cluster_name,
+    "AIRFLOW_VAR_WRK_NAMESPACE" : local.composer_work_namespace,
   }
 }
 
 module "dbt_docker_image" {
   depends_on         = [module.composer]
-  source             = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/dbt_docker_image"
+  source             = "./modules/dbt_docker_image"
   registry_hostname  = module.gcr.registry_hostname
   registry_repo_name = coalesce(var.project_name)
   project_name       = var.project_name
   spark_version      = local.spark_version
-  dbt_spark_version  = local.spark_version
+  dbt_version        = local.dbt_version
+  dbt_spark_version  = local.dbt_spark_version
 }
 
 module "data-pipelines" {
-  source               = "github.com/bdg-tbd/tbd-workshop-1.git?ref=v1.0.34/modules/data-pipeline"
+  source               = "./modules/data-pipeline"
   project_name         = var.project_name
   region               = var.region
   bucket_name          = local.code_bucket_name
@@ -103,3 +112,46 @@ module "data-pipelines" {
 
 
 
+
+resource "kubernetes_service" "dbt-task-service" {
+  metadata {
+    name      = "dbt-task-service"
+    namespace = local.composer_work_namespace
+    labels = {
+      app = "dbt-app"
+    }
+  }
+
+  spec {
+    type = "NodePort"
+    selector = {
+      app = "dbt-app"
+    }
+    port {
+      name        = "spark-driver"
+      protocol    = "TCP"
+      port        = local.spark_driver_port
+      target_port = local.spark_driver_port
+      node_port   = local.spark_driver_port
+
+    }
+    port {
+      name        = "spark-block-mgr"
+      protocol    = "TCP"
+      port        = local.spark_blockmgr_port
+      target_port = local.spark_blockmgr_port
+      node_port   = local.spark_blockmgr_port
+    }
+
+  }
+}
+
+resource "google_compute_firewall" "allow-all-internal" {
+  name    = "allow-all-internal"
+  project = var.project_name
+  network = module.vpc.network.network_name
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["10.0.0.0/8"]
+}
