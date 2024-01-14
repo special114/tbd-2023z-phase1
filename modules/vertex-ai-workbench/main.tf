2e583289aca6f82e063eba6523901006b8613c5c
Rafał Surdej <special114@mailbox.org>
2024-01-14 15:48:28 +0100

    Add checkov skip

--- modules/vertex-ai-workbench/main.tf
+++ modules/vertex-ai-workbench/main.tf
@@ -48,6 +48,8 @@ resource "google_storage_bucket_object" "post-startup" {
 
 resource "google_notebooks_instance" "tbd_notebook" {
   #checkov:skip=CKV2_GCP_18: "Ensure GCP network defines a firewall and does not use the default firewall"
+  #checkov:skip=CKV2_GCP_21: "Ensure Vertex AI instance disks are encrypted with a Customer Managed Key (CMK)"
+  
   depends_on   = [google_project_service.notebooks]
   location     = local.zone
   machine_type = var.machine_type
@@ -59,7 +61,7 @@ resource "google_notebooks_instance" "tbd_notebook" {
   network = var.network
   subnet  = var.subnet
   
-  # Enable Secure Boot
+  # Enable Secure Bootlled and 
   shielded_instance_config {
     enable_secure_boot = true
   }
